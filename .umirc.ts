c22849730c66c16f2e14d81cc50bcb3786e88f69
Liu Beng <liubeng668@gmail.com>
2021-01-21 14:39:50 +0800

    push

--- .umirc.ts
+++ .umirc.ts
@@ -3,14 +3,13 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: '重学ES6',
   mode: 'doc',
-  exportStatic: {}
   // routes: [
   //   { path: '/', component: './src/index.md' },
   //   { path: '/let-const', component: './src/let-const.md' },
   //   { path: '/destructuring', component: './src/destructuring.md' },
   // ],
-  // history: {
-  //   type: 'hash'
-  // }
+  history: {
+    type: 'hash'
+  }
   // more config: https://d.umijs.org/config
 });
